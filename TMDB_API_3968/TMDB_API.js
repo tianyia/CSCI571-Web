a1e8684c9a3e8bf5e2c2b5cb95c829481581df0a
tianyia <55004002+tianyia@users.noreply.github.com>
2021-02-21 20:48:44 -0800

    Update TMDB_API.js

--- TMDB_API_3968/TMDB_API.js
+++ TMDB_API_3968/TMDB_API.js
@@ -1,1 +1,26 @@
+function switch_tab(evt, content_name)
+{
+	var tab, content;
+	content = document.getElementsByClassName("content");
+	for (var i = 0; i < 2; i++) 
+	{
+		content[i].style.display = "none";
+	}
 
+	if (content_name == "Home")
+	{
+		content[0].style.display = "inline-block";
+	}
+	else
+	{
+		content[1].style.display = "inline-block";
+	}
+	
+  	tab = document.getElementsByClassName("tab");
+	for (var i = 0; i < 2; i++) 
+	{
+		tab[i].className = tab[i].className.replace(" active", ""); /*string method .replace*/
+	}
+  	
+  	evt.currentTarget.className += " active";
+}
