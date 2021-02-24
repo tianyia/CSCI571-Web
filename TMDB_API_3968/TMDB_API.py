f653a71248fcec18e98c76cb33c09b0d134783c0
tianyia <55004002+tianyia@users.noreply.github.com>
2021-02-23 16:59:19 -0800

    Update TMDB_API.py

--- TMDB_API_3968/TMDB_API.py
+++ TMDB_API_3968/TMDB_API.py
@@ -1,1 +1,15 @@
+from flask import Flask, make_response, request, jsonify
 
+
+#create Flask's app object
+app = Flask(__name__)
+
+#execute this function when a user visits app domain at given route
+#stacking multiple routes here for 1 function is possible
+@app.route("/", methods=['GET'])
+def hello():
+	headers = {"Content-Type": "application/json"}
+	return make_response(
+		'Server works!!! omedeto!',
+		200,
+		headers=headers)
