eb83276a246a3b3ffb474517dc26f0c1484bb262
tianyia <55004002+tianyia@users.noreply.github.com>
2021-02-23 16:13:58 -0800

    Update TMDB_API.js

--- TMDB_API_3968/TMDB_API.js
+++ TMDB_API_3968/TMDB_API.js
@@ -29,14 +29,34 @@ function get_home_pics()
 {
   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() 
+  xhttp.onreadystatechange = function() /*callback is the function to be executed when the readyState changes*/
   {
-    if (xhttp.readyState == 4 && xhttp.status == 200) 
+    if (xhttp.readyState == 4 && xhttp.status == 200) /*request state and server status*/
     {
-      document.getElementById("Home_img1").innerHTML = xhttp.responseText;
+      	var Home_img1 = document.getElementsByClassName("Home_img1")
+		for (var i = 0; i < 5; i++) 
+		{
+			Home_img1[i].src = "link for image";/*this.responseText;*/
+		}
     }
   };
 
   xhttp.open("GET", "server address", true);
   xhttp.send();
+}
+
+var slide_ind = 0
+
+function Home_slideshow
+{
+	var Home_slide1 = document.getElementsByClassName("Home_slide1");
+	var Home_slide2 = document.getElementsByClassName("Home_slide2");
+  	for (i = 0; i < Home_slide1.length; i++) 
+  	{
+  		Home_slide1[i].style.display = "none";
+  		Home_slide2[i].style.display = "none";
+  	}
+  	Home_slide1[slide_ind].style.display = "block";
+  	Home_slide2[slide_ind].style.display = "block";
+  	setTimeout(Home_slideshow, 5000); /*change every 5 seconds*/
 }
