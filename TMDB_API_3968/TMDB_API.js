99f23c1be6ecc391ac5f3b0dc8bbc788d4f90e0e
tianyia <55004002+tianyia@users.noreply.github.com>
2021-02-24 20:05:45 -0800

    Update TMDB_API.js

--- TMDB_API_3968/TMDB_API.js
+++ TMDB_API_3968/TMDB_API.js
@@ -33,33 +33,53 @@ function get_home_pics()
   {
     if (xhttp.readyState == 4 && xhttp.status == 200) /*request state and server status*/
     {
-      	//var Home_img1 = document.getElementsByClassName("Home_img1");
-		//for (var i = 0; i < 5; i++) 
-		//{
-			//Home_img1[i].src = "link for image"; //this.responseText;
-		//}
-		var test = document.getElementsByClassName("test"); //getElements returns array like data
-		test[0].innerHTML = this.responseText;
+    	var image_data = JSON.parse(this.responseText)
+      	var Home_img1 = document.getElementsByClassName("Home_img1");
+      	var Home_img2 = document.getElementsByClassName("Home_img2");
+
+      	var Home_des1 = document.getElementsByClassName("Home_des1");
+      	var Home_des2 = document.getElementsByClassName("Home_des2");
+		for (var i = 0; i < 5; i++) 
+		{
+			Home_img1[i].src = "https://image.tmdb.org/t/p/w780" + image_data[i]['backdrop_path'];
+			Home_img2[i].src = "https://image.tmdb.org/t/p/w780" + image_data[i+5]['backdrop_path'];
+
+			Home_des1[i].innerHTML = image_data[i]['title'] + "(" + image_data[i]['release_date'].slice(0,4) + ")";
+			Home_des2[i].innerHTML = image_data[i+5]['name'] + "(" + image_data[i+5]['first_air_date'].slice(0,4) + ")";
+		}
+		//var test = document.getElementsByClassName("test"); //getElements returns array like data
+		//test[0].innerHTML = this.responseText;
+
+		//document.getElementById('movie-title').innerHTML = obj['original_title'];
     }
   };
 
-  xhttp.open("GET", "http://localhost:5000/", true);
+  xhttp.open("GET", "http://localhost:5000/home", true);
   xhttp.send();
 }
 get_home_pics();
 
 var slide_ind = 0
 
-/*function Home_slideshow
+function Home_slideshow()
 {
-	var Home_slide1 = document.getElementsByClassName("Home_slide1");
-	var Home_slide2 = document.getElementsByClassName("Home_slide2");
-  	for (i = 0; i < Home_slide1.length; i++) 
+	var Home_figure1 = document.getElementsByClassName("Home_figure1");
+	var Home_figure2 = document.getElementsByClassName("Home_figure2");
+  	for (var i = 0; i < Home_figure1.length; i++) 
   	{
-  		Home_slide1[i].style.display = "none";
-  		Home_slide2[i].style.display = "none";
+  		Home_figure1[i].style.display = "none";
+  		Home_figure2[i].style.display = "none";
   	}
-  	Home_slide1[slide_ind].style.display = "block";
-  	Home_slide2[slide_ind].style.display = "block";
+  	Home_figure1[slide_ind].style.display = "block";
+  	Home_figure2[slide_ind].style.display = "block";
+
+  	slide_ind++;
+  	if (slide_ind==5)
+  	{
+  		slide_ind = 0;
+  	}
+
   	setTimeout(Home_slideshow, 5000); //change every 5 seconds
-}*/
+}
+
+Home_slideshow();
