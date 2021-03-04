e4ac2a3842bffac34c3d0e3e293d1dd48d02ac19
tianyia <55004002+tianyia@users.noreply.github.com>
2021-03-04 15:10:10 -0800

    Update Tyi3968.js

--- Tyi3968.js
+++ Tyi3968.js
@@ -1,1 +1,8 @@
+function set_links()
+{
+	var link = document.getElementsByClassName("link");
 
+	link[0].href = "https://tianyia.github.io/website_mimic_3968/hw3_website_mimic.html";
+	link[1].href = "https://tianyia.github.io/TMDB_API_3968/TMDB_API.html";
+}
+set_links();
