6e7c71be001736a6c60809b9fc91d83204f1c4e1
tianyia <55004002+tianyia@users.noreply.github.com>
2021-03-07 12:43:33 -0800

    Update Tyi3968.js

--- Tyi3968.js
+++ Tyi3968.js
@@ -3,6 +3,7 @@ function set_links()
 	var link = document.getElementsByClassName("link");
 
 	link[0].href = "https://tianyia.github.io/website_mimic_3968/hw3_website_mimic.html";
-	link[1].href = "https://tianyia.github.io/TMDB_API_3968/TMDB_API.html";
+	link[1].href = "https://tianyi-tmdb-api.azurewebsites.net/";
+	link[1].href = "https://tianyi-tmdb-api.azurewebsites.net/home";
 }
 set_links();
