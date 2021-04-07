6a1803aee470756ff2032d91cb6602916e115550
tianyia <55004002+tianyia@users.noreply.github.com>
2021-04-07 01:27:03 -0700

    Update Tyi3968.js

--- Tyi3968.js
+++ Tyi3968.js
@@ -6,10 +6,12 @@ function set_links()
 	link[1].href = "https://tianyi-tmdb-api.azurewebsites.net/";
 	link[2].href = "https://tianyi-tmdb-api.azurewebsites.net/home";
 	link[3].href = "https://advanced-moviedb-by-tianyi-an.wl.r.appspot.com/";
+	link[4].href = "https://advanced-moviedb-by-tianyi-an.wl.r.appspot.com/apis/watch/movie/284052";
 
 	link[0].target = "_blank";
 	link[1].target = "_blank";
 	link[2].target = "_blank";
 	link[3].target = "_blank";
+	link[4].target = "_blank";
 }
 set_links();
