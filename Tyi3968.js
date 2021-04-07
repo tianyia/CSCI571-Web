8bdfe43a62b8fe5d001bab4ad58d66d2ba867a0c
tianyia <55004002+tianyia@users.noreply.github.com>
2021-04-07 01:07:16 -0700

    Update Tyi3968.js

--- Tyi3968.js
+++ Tyi3968.js
@@ -5,9 +5,11 @@ function set_links()
 	link[0].href = "https://tianyia.github.io/website_mimic_3968/hw3_website_mimic.html";
 	link[1].href = "https://tianyi-tmdb-api.azurewebsites.net/";
 	link[2].href = "https://tianyi-tmdb-api.azurewebsites.net/home";
+	link[3].href = "https://advanced-moviedb-by-tianyi-an.wl.r.appspot.com/";
 
 	link[0].target = "_blank";
 	link[1].target = "_blank";
 	link[2].target = "_blank";
+	link[3].target = "_blank";
 }
 set_links();
