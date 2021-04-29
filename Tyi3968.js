43f5b87b38d9b523495e0238fd573708ab1ae7ba
tianyia <55004002+tianyia@users.noreply.github.com>
2021-04-28 18:07:15 -0700

    Update Tyi3968.js

--- Tyi3968.js
+++ Tyi3968.js
@@ -7,11 +7,13 @@ function set_links()
 	link[2].href = "https://tianyi-tmdb-api.azurewebsites.net/home";
 	link[3].href = "https://advanced-moviedb-by-tianyi-an.wl.r.appspot.com/";
 	link[4].href = "https://advanced-moviedb-by-tianyi-an.wl.r.appspot.com/apis/watch/movie/284052";
+	link[5].href = "http://moviedbandroidserverbytianyian-env.eba-khpg3mej.us-east-2.elasticbeanstalk.com/apis";
 
 	link[0].target = "_blank";
 	link[1].target = "_blank";
 	link[2].target = "_blank";
 	link[3].target = "_blank";
 	link[4].target = "_blank";
+	link[5].target = "_blank";
 }
 set_links();
